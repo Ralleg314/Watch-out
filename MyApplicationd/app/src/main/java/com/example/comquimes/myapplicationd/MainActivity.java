f8d2f22919fdf757572a5a000f0252ea168491b0
comquimes <quimcu@gmail.com>
2017-10-14 17:57:23 +0200

    soy guay

---
+++ MyApplicationd/app/src/main/java/com/example/comquimes/myapplicationd/MainActivity.java
@@ -0,0 +1,182 @@
+package com.example.comquimes.myapplicationd;
+
+import android.Manifest;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Address;
+import android.location.Geocoder;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.location.LocationProvider;
+import android.provider.Settings;
+import android.support.v4.app.ActivityCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.app.NotificationCompat;
+import android.util.Log;
+import android.widget.TextView;
+
+import com.google.android.gms.maps.MapView;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.Locale;
+
+public class MainActivity extends AppCompatActivity {
+    TextView mensaje1;
+    TextView mensaje2;
+    TextView mensaje3;
+    NotificationManager mNotifyMgr;
+    Notification notification;
+    MapView mView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mNotifyMgr = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
+        notification = new Notification(this);
+
+        mensaje1 = (TextView) findViewById(R.id.mensaje_id);
+        mensaje2 = (TextView) findViewById(R.id.mensaje_id2);
+        mensaje3 = (TextView) findViewById(R.id.txt_info);
+
+        mView = (MapView) findViewById(R.id.mView);
+
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION,}, 1000);
+        } else {
+            locationStart();
+        }
+
+        mView.setActivated(true);
+
+        //Intent intent = new Intent(MainActivity.this,map.class);
+        //startActivity(intent);
+    }
+
+    private void locationStart() {
+        LocationManager mlocManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+        Localizacion Local = new Localizacion();
+        Local.setMainActivity(this);
+        final boolean gpsEnabled = mlocManager.isProviderEnabled(LocationManager.GPS_PROVIDER);
+        if (!gpsEnabled) {
+            Intent settingsIntent = new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS);
+            startActivity(settingsIntent);
+        }
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION,}, 1000);
+            return;
+        }
+        mlocManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 0, 0, (LocationListener) Local);
+        mlocManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, (LocationListener) Local);
+
+        mensaje1.setText("Localizacion agregada");
+        mensaje2.setText("");
+    }
+
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        if (requestCode == 1000) {
+            if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                locationStart();
+                return;
+            }
+        }
+    }
+
+    public void setLocation(Location loc) {
+        //Obtener la direccion de la calle a partir de la latitud y la longitud
+        if (loc.getLatitude() != 0.0 && loc.getLongitude() != 0.0) {
+            try {
+                Geocoder geocoder = new Geocoder(this, Locale.getDefault());
+                List<Address> list = geocoder.getFromLocation(
+                        loc.getLatitude(), loc.getLongitude(), 1);
+                if (!list.isEmpty()) {
+                    Address DirCalle = list.get(0);
+                    mensaje2.setText("Mi direccion es: \n"
+                            + DirCalle.getAddressLine(0));
+                }
+
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+    /* Aqui empieza la Clase Localizacion */
+    public class Localizacion implements LocationListener {
+        MainActivity mainActivity;
+
+        public MainActivity getMainActivity() {
+            return mainActivity;
+        }
+
+        public void setMainActivity(MainActivity mainActivity) {
+            this.mainActivity = mainActivity;
+        }
+
+        @Override
+        public void onLocationChanged(Location loc) {
+            // Este metodo se ejecuta cada vez que el GPS recibe nuevas coordenadas
+            // debido a la deteccion de un cambio de ubicacion
+
+            loc.getLatitude();
+            loc.getLongitude();
+
+            double latitude = 41.38895083;
+            double longitude = 2.1135003;
+
+            double radioTierra = 6371;//en kilómetros
+            double dLat = Math.toRadians(latitude - loc.getLatitude());
+            double dLng = Math.toRadians(longitude - loc.getLongitude());
+            double sindLat = Math.sin(dLat / 2);
+            double sindLng = Math.sin(dLng / 2);
+            double va1 = Math.pow(sindLat, 2) + Math.pow(sindLng, 2)
+                    * Math.cos(Math.toRadians(loc.getLatitude())) * Math.cos(Math.toRadians(latitude));
+            double va2 = 2 * Math.atan2(Math.sqrt(va1), Math.sqrt(1 - va1));
+            double distancia = radioTierra * va2 * 1000;
+
+            String Text = "Mi ubicacion actual es: " + "\n Lat = "
+                    + loc.getLatitude() + "\n Long = " + loc.getLongitude();
+            mensaje1.setText(Text);
+            this.mainActivity.setLocation(loc);
+            if (distancia <= 15) {
+                mensaje3.setText("Estas al punt d'interes");
+                mNotifyMgr.notify(notification.getID(),notification.getmBuilder().build());
+            } else {
+                mensaje3.setText("Estas a " + Double.toString(Math.round(distancia) ) + " metros del punt d'interes");
+            }
+        }
+
+        @Override
+        public void onProviderDisabled(String provider) {
+            // Este metodo se ejecuta cuando el GPS es desactivado
+            mensaje1.setText("GPS Desactivado");
+        }
+
+        @Override
+        public void onProviderEnabled(String provider) {
+            // Este metodo se ejecuta cuando el GPS es activado
+            mensaje1.setText("GPS Activado");
+        }
+
+        @Override
+        public void onStatusChanged(String provider, int status, Bundle extras) {
+            switch (status) {
+                case LocationProvider.AVAILABLE:
+                    Log.d("debug", "LocationProvider.AVAILABLE");
+                    break;
+                case LocationProvider.OUT_OF_SERVICE:
+                    Log.d("debug", "LocationProvider.OUT_OF_SERVICE");
+                    break;
+                case LocationProvider.TEMPORARILY_UNAVAILABLE:
+                    Log.d("debug", "LocationProvider.TEMPORARILY_UNAVAILABLE");
+                    break;
+            }
+        }
+    }
+}
---
+++ MyApplicationd/app/src/main/java/com/example/comquimes/myapplicationd/Notification.java
@@ -0,0 +1,32 @@
+package com.example.comquimes.myapplicationd;
+
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Intent;
+import android.support.v4.app.NotificationCompat;
+
+/**
+ * Created by comquimes on 14/10/17.
+ */
+
+public class Notification {
+    int mNotificationID;
+    NotificationCompat.Builder mBuilder;
+    public Notification(MainActivity mainActivity) {
+        mNotificationID = 999;
+        mBuilder = new NotificationCompat.Builder(mainActivity)
+                //.setSmallIcon(android.R.drawable.ic_notification_clear_all)
+                .setSmallIcon(R.drawable.upc)
+                .setContentTitle("Watch Out")
+                .setContentText("Estas muy cerca de un punto de interes!") ;
+
+        Intent resultIntent = new Intent(mainActivity,mainActivity.getClass());
+
+        PendingIntent resultPendingIntent = PendingIntent.getActivity(mainActivity,0,resultIntent,PendingIntent.FLAG_UPDATE_CURRENT);
+
+        mBuilder.setContentIntent(resultPendingIntent);
+    }
+    public int getID(){return mNotificationID;}
+
+    public NotificationCompat.Builder getmBuilder() {return mBuilder;}
+}
